a30745291325010d5979bce10e5b8c987abe25db
alu0100905144 <32196510+alu0100905144@users.noreply.github.com>
2019-04-03 13:31:57 +0100

    Count the smiley faces!

---
+++ src/test/java/com/tdd/SmileyCheckerTest.java
@@ -0,0 +1,66 @@
+package com.tdd;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class SmileyCheckerTest {
+
+    private SmileyChecker smileyChecker;
+
+    @Before
+    public void initializer(){
+        smileyChecker = new SmileyChecker();
+    }
+
+    public int countSmileys(String[] smileys){
+        int counter = 0;
+        for(String smiley : smileys){
+            if (smileyChecker.isASmiley(smiley)){
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+
+    @Test
+    public void emptyEntryIsNotASmiley(){
+        Assert.assertFalse(smileyChecker.isASmiley(""));
+    }
+
+    @Test
+    public void sadFaceIsNotASmiley(){
+        Assert.assertFalse(smileyChecker.isASmiley(":("));
+    }
+
+    @Test
+    public void HappyFaceIsASmiley(){
+        Assert.assertTrue(smileyChecker.isASmiley(":)"));
+        Assert.assertTrue(smileyChecker.isASmiley(":D"));
+    }
+
+    @Test
+    public void winkFaceIsASmiley(){
+        Assert.assertTrue(smileyChecker.isASmiley(";)"));
+    }
+
+    @Test
+    public void smileyCanHaveNose(){
+        Assert.assertTrue(smileyChecker.isASmiley(":-D"));
+        Assert.assertTrue(smileyChecker.isASmiley(";~)"));
+    }
+
+    @Test
+    public void smileysAreDetected(){
+        String[] smileys =  {":)", ";(", ";}", ":-D"};
+        Assert.assertEquals(2, countSmileys(smileys));
+
+        smileys = new String[] {";D", ":-(", ":-)", ";~)"};
+        Assert.assertEquals(3, countSmileys(smileys));
+
+        smileys = new String[] {";]", ":[", ";*", ":$", ";-D"};
+        Assert.assertEquals(1, countSmileys(smileys));
+    }
+
+}
